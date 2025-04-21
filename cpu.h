cc21f341c37b4108566171fc96cc4eca4d5f0371
Joshua James <joshjame@cs.utexas.edu>
2025-04-21 15:55:16 -0500

    added header files

---
+++ cpu.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <fstream>
+#include <stdio.h>
+#include "ppu.h"
---
+++ ppu.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <chrono>
+#include <SDL2/SDL.h>
+#include "cpu.h"
+
+void PPU_cycle();
